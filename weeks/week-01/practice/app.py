94d02920ccab310bdb566b39128a1eaa461a49a6
lo9876576435 <24020203@vnu.edu.vn>
2026-09-10 22:30:48 +0700

    feat: implement ex3

--- weeks/week-01/practice/app.py
+++ weeks/week-01/practice/app.py
@@ -1,18 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from uuid import uuid4
 
 app = Flask(__name__)
 
-@app.route("/health", method = ["GET"])
-def health():
-    return jsonify({"status": "ok"}), 200
+STUDENTS = []
 
-@app.route("/echo", method = ["POST"])
+@app.route("/students", methods = ["POST"])
+def create_student():
+    body = request.get_json(silent = True) or {}
+    name = body.get("name")
+    if not name:
+        return jsonify({"error": "Tên là bắt buộc"}), 400
+    student = {
+        "id": str(uuid4()),
+        "name": name,
+        "gpa": body.get("gpa", 0.0)
+    }
+    STUDENTS.append(student)
+    return jsonify({"id": student["id"], "name": student["name"]}), 201
+
+@app.route("/health", methods = ["GET"])
+def health():
+    return jsonify({"status": "ok"}), 201 
+
+@app.route("/echo", methods = ["POST"])
 def echo():
-    data = request.get_json(slient = True) or {} 
+    data = request.get_json(silent = True) or {} 
     ## silent = True --> nhập đúng định dạng json mới chấp nhận
     return jsonify({"you sent": data}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-## request: curl -X POST http://127.0.0.1:5000/echo \-H "Content-Type: application/json" \-d '{"name": "An", "age": "21"}'
+    app.run(host="0.0.0.0", port=5001, debug=True)
